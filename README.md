61000eb7e76438b60816c4b886171ed0b944be6b
developerpaddy <developerpaddy@gmail.com>
2021-04-22 20:36:50 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cp-filter
+Wordpress Post filter
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: CP Filter
+ * Description: Wordpress filter plugin.
+ * Version: 0.0.1
+ * Author: creativepaddy
+ * Plugin Slug: cp-filter
+ */
